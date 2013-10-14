9bf1618b3687c36ec2af5b93e1293938c1d856af
Paweł Matyszok <pawel.matyszok@gmail.com>
2013-10-15 00:24:17 +0200

    Fixes in includes and string literal according to Marcin's code review.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
-#include <gtest/gtest.h>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "My super sweet Ground Control Station" << endl;
+	cout << "My super sweet Ground Control Station." << endl;
 	return 0;
 }
