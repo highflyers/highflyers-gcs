354977c0682363dba6b0dd9cc11b86cf814c1d04
Marcin Kolny <marcin.kolny@gmail.com>
2013-10-23 23:14:56 +0200

    trying to make cross-compile loader

--- src/core/PluginLoader.cpp
+++ src/core/PluginLoader.cpp
@@ -5,8 +5,14 @@
  *      Author: Marcin Kolny
  */
 
-#include "core/PluginLoader.h";
+#include "core/PluginLoader.h"
+#ifdef __linux__
 #include <dlfcn.h>
+#elif defined _WIN32
+#include <windows.h>
+#else
+	static_assert(false, "Unsupported OS.");
+#endif
 #include <stdexcept>
 
 using namespace HighFlyers;
@@ -38,7 +44,14 @@ void _PluginLoader::open_plugin(const std::string& filename)
 	if (is_plugin_loaded(filename))
 		return;
 
-	void* lib = dlopen(filename.c_str(), RTLD_NOW);
+	void* lib =
+#ifdef __linux__
+			dlopen(filename.c_str(), RTLD_NOW);
+#elif defined _WIN32
+			reinterpret_cast<void*>(LoadLibrary(filename.c_str()));
+#else
+	nullptr; static_assert(false, "Unsupported OS.");
+#endif
 
 	if (lib == nullptr)
 		throw std::runtime_error("Cannot load plugin " +
@@ -52,7 +65,14 @@ void _PluginLoader::close_plugin(const std::string& filename)
 	if (!is_plugin_loaded(filename))
 		return;
 
-	int ret = dlclose(libraries[filename]);
+	int ret =
+#ifdef __linux__
+			dlclose(libraries[filename]);
+#elif defined _WIN32
+			FreeLibrary(reinterpret_cast<HMODULE>(libraries[filename]));
+#else
+			0; static_assert(false, "Unsupported OS.");
+#endif
 
 	if (!ret)
 		throw std::runtime_error("Cannot close plugin " +
