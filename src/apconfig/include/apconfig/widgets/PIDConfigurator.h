58405ed2a43bf4e8d64777bda2e9c6376c03a66e
Marcin Kolny <marcin.kolny@gmail.com>
2013-10-25 14:21:25 +0200

    added doxygen configuration file
    added one more field in pid config struct
    
    implement get/set methods
    
    updated coding style document - something more about comments

--- src/apconfig/include/apconfig/widgets/PIDConfigurator.h
+++ src/apconfig/include/apconfig/widgets/PIDConfigurator.h
@@ -10,6 +10,7 @@
 
 #include <map>
 #include <QLineEdit>
+#include <QLocale>
 
 namespace HighFlyers
 {
@@ -18,18 +19,30 @@ class PIDConfigurator : public QWidget
 private:
 	static const char* pid_names[3];
 	std::map<const char*, QLineEdit*> edits;
-
+	QLocale loc;
 	QStringList pid_items = {"AcroRoll", "AcroPitch", "AcroYaw", "StableRoll", "StablePitch", "Alt", "Vel"};
+
+	void set_value(const char* l, double value);
+	double get_value(const char* l);
+
 public:
 	PIDConfigurator(QWidget* parent = 0);
 
 	template<typename T>
 	void set(const char* label, const T& value)
-	{}
+	{
+		if (edits.count(label))
+		{
+			set_value(label, value);
+		}
+	}
 
 	template<typename T>
 	T get(const char* label)
 	{
+		if (edits.count(label))
+			return get_value(label);
+
 		return T();
 	}
 };
--- src/apconfig/widgets/PIDConfigurator.cpp
+++ src/apconfig/widgets/PIDConfigurator.cpp
@@ -10,13 +10,15 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QComboBox>
+#include <stdexcept>
 
 using namespace HighFlyers;
 
 const char* PIDConfigurator::pid_names[] = {"P", "I", "D"};
 
 PIDConfigurator::PIDConfigurator(QWidget* parent)
-: QWidget(parent)
+: QWidget(parent),
+  loc(QLocale::Polish, QLocale::Poland)
 {
 	QVBoxLayout* main_layout = new QVBoxLayout();
 
@@ -31,8 +33,25 @@ PIDConfigurator::PIDConfigurator(QWidget* parent)
 
 	QComboBox* pid_combo = new QComboBox();
 
-	pid_combo->addItem("pid_items");
+	pid_combo->addItems(pid_items);
+
 	main_layout->addWidget(pid_combo);
 
 	setLayout(main_layout);
+}
+
+void PIDConfigurator::set_value(const char* l, double value)
+{
+	edits[l]->setText(loc.toString(value));
+}
+
+double PIDConfigurator::get_value(const char* l)
+{
+	bool ok;
+	double v = loc.toDouble(edits[l]->text(), &ok);
+
+	if (!ok)
+		throw std::runtime_error("Cannot get value from PID configurator.");
+
+	return v;
 }
