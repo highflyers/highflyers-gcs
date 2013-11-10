e8fa3b46ce6a837db67897558ecd22a31aee538f
Paweł Matyszok <pawel.matyszok@gmail.com>
2013-11-10 15:32:01 +0100

    corrected whitespaces, usage delete-function syntax

--- src/core/include/core/Singleton.h
+++ src/core/include/core/Singleton.h
@@ -2,25 +2,25 @@
 #define SINGLETONPATTERN_H
 namespace HighFlyers
 {
-    template <typename T>
-    class Singleton
-    {
-    public:
-        static T* get_instance()
-        {
-            if (instance == nullptr)
-                instance = new T();
-            return instance;
-        }
-    protected:
-        Singleton();
-        ~Singleton();
-    private:
-        Singleton(const Singleton&);
-        Singleton& operator=(const Singleton&);
-        static T* instance;
-    };
-    template <typename T> T* Singleton<T>::instance = nullptr;
+	template <typename T>
+	class Singleton
+	{
+	public:
+		static T* get_instance()
+		{
+			if (instance == nullptr)
+				instance = new T();
+			return instance;
+		}
+	protected:
+		Singleton();
+		~Singleton();
+	private:
+		Singleton(const Singleton&) = delete;
+		Singleton& operator=(const Singleton&) = delete;
+		static T* instance;
+	};
+	template <typename T> T* Singleton<T>::instance = nullptr;
 }
 
 #endif // SINGLETONPATTERN_H
