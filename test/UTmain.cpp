4db363bb842c8451d9016dec22f68a8e40d0e8d2
Marcin Kolny <marcin.kolny@gmail.com>
2013-11-11 23:51:26 +0100

    fixed tests running

--- test/UTmain.cpp
+++ test/UTmain.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
+#include <QApplication>
 
 int main(int argc, char** argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
+	QApplication app(argc, argv);
 
 	return RUN_ALL_TESTS();
 }
