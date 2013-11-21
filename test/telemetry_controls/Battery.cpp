d8504360bd0e41faaf0464b43c28b2c9c051c2a2
Maciej Smolarczyk <brayant@op.pl>
2013-11-21 19:33:36 +0100

    fixed code style

--- test/telemetry_controls/Battery.cpp
+++ test/telemetry_controls/Battery.cpp
@@ -5,8 +5,8 @@ TEST( BatteryTest, CheckSetMaxValue )
 {
 	float expected = 42.f;
 	Battery_Control battery;
-	battery.set_maxvalue(expected);
-	ASSERT_FLOAT_EQ(battery.get_maxvalue(), expected);
+	battery.set_maxvalue( expected );
+	ASSERT_FLOAT_EQ( battery.get_maxvalue(), expected );
 }
 
 TEST( BatteryTest, CheckReturn_procent )
@@ -14,28 +14,29 @@ TEST( BatteryTest, CheckReturn_procent )
 	int expected = 0;
 	Battery_Control bat;
 
-	for (int i=0 ; i<90 ; i++){
-		bat.set_maxvalue(i+23);
-		bat.set_minvalue(i);
-		bat.set_value(i+11);
-		expected = ((bat.get_value()-bat.get_minvalue())*100/(bat.get_maxvalue()-bat.get_minvalue()));
+	for (int i=0; i < 90; i++)
+	{
+		bat.set_maxvalue( i + 23 );
+		bat.set_minvalue( i );
+		bat.set_value( i + 11 );
+		expected = (( bat.get_value() - bat.get_minvalue() ) * 100 / ( bat.get_maxvalue() - bat.get_minvalue() ) );
 
-		ASSERT_EQ( bat.return_procent(),expected );
+		ASSERT_EQ( bat.return_procent(), expected );
 	}
 }
 
 TEST( BatteryTest, CheckReturn_procentForZeros )
-	{
-		int expected = 0;
-		Battery_Control batt;
-		batt.set_maxvalue(0);
-		batt.set_minvalue(0);
-		batt.set_value(0);
+{
+	int expected = 0;
+	Battery_Control batt;
+	batt.set_maxvalue( 0 );
+	batt.set_minvalue( 0 );
+	batt.set_value( 0 );
 
-		ASSERT_EQ(batt.return_procent(),expected);
+	ASSERT_EQ( batt.return_procent(), expected );
 
 
-	}
+}
 
 
 
