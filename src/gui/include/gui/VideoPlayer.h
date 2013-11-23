e136142935b1d1e6d9c7164134f52bd16f73390a
Marcin Kolny <marcin.kolny@gmail.com>
2013-11-23 13:14:30 +0100

    improved videoplayer added play and stop button

--- src/gui/include/gui/VideoPlayer.h
+++ src/gui/include/gui/VideoPlayer.h
@@ -15,10 +15,17 @@ class VideoPlayer : public QWidget
 	Q_OBJECT
 
 private:
+	QFrame* image_canvas;
 
 public:
 	virtual ~VideoPlayer() {}
 	VideoPlayer( QWidget* parent = 0 );
+
+	WId get_canvas_handler();
+
+Q_SIGNALS:
+	void state_clicked( bool play );
+	void config_window_requested();
 };
 
 #endif /* VIDEOPLAYER_H_ */
