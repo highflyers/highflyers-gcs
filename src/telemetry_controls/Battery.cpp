449791deeb1d8660d1418cdd2332e123782cf6f4
Wojciech Dudzik <wojtek.adam.dudzik@gmail.com>
2013-11-23 23:18:46 +0100

    fixed build error, added namespace, changes acording to code review, changed code styling in some places

--- src/telemetry_controls/Battery.cpp
+++ src/telemetry_controls/Battery.cpp
@@ -7,18 +7,19 @@
 #include <QHBoxLayout>
 #include <QtGui>
 
-Battery::Battery(QWidget *parent) :
-	QWidget(parent),
-	ui(new Ui::Battery)
+
+using namespace HighFlyers;
+
+Battery::Battery( QWidget *parent ) :
+	QWidget( parent ),
+	ui( new Ui::Battery )
 {
-	ui->setupUi(this);
+	ui->setupUi( this );
 	max = new QLabel;
 	min = new QLabel;
 	actual = new QLabel;
 	volt = new QLabel;
-	status = new Batterybar;
-
-
+	status = new BatteryBar;
 }
 
 
@@ -32,144 +33,154 @@ Battery::~Battery()
 	delete status;
 }
 
-void Battery::paintEvent(QPaintEvent *event)
+void Battery::paintEvent( QPaintEvent *event )
 {
 	QHBoxLayout *widget = new QHBoxLayout;
 
 	delete this->layout();
-	this->setLayout(widget);		// setting layout to widget
+	this->setLayout( widget );		// setting layout to widget
 
 	QHBoxLayout *rest = new QHBoxLayout;
 	QVBoxLayout *left = new QVBoxLayout;
 	QVBoxLayout *right = new QVBoxLayout;
 
-	widget->addLayout(rest);	// adding layout witch holds left and right layouts
-	rest->addLayout(left);
-	rest->addLayout(right);
+	widget->addLayout( rest );	// adding layout witch holds left and right layouts
+	rest->addLayout( left );
+	rest->addLayout( right );
 
+	left->addWidget( max );
+	left->setAlignment( max,Qt::AlignTop | Qt::AlignCenter );
 
-	left->addWidget(max);
-	left->setAlignment(max,Qt::AlignTop |Qt::AlignCenter);
+	left->addWidget( status );	// here is added progressbar
+	status->setAlignment( Qt::AlignHCenter | Qt::AlignTop );
 
-	left->addWidget(status);	// here is added progress bar
-	status->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+	left->setStretch( 0,0 );		// these instructions set the stretch so progressbar can have right dimensions
+	left->setStretch( 1,10 );
 
-	left->setStretch(0,0);		// these instructions set the stretch so progress bar can have right dimensions
-	left->setStretch(1,10);
+	left->addWidget( min );
+	min->setAlignment( Qt::AlignBottom | Qt::AlignCenter );
 
-	left->addWidget(min);
-	min->setAlignment(Qt::AlignBottom |Qt::AlignCenter);
+	volt->setText( "Voltage:" );
+	right->addWidget( volt );
+	right->setAlignment( volt,Qt::AlignBottom );
 
-	volt->setText("Voltage:");
-	right->addWidget(volt);
-	right->setAlignment(volt,Qt::AlignBottom);
+	right->addWidget( actual );
+	actual->setAlignment( Qt::AlignTop );
 
-	right->addWidget(actual);
-	actual->setAlignment(Qt::AlignTop);
-
-	this->setMinimumHeight(200);
-	this->setMinimumWidth(200);
+	this->setMinimumHeight( 200 );
+	this->setMinimumWidth( 200 );
 }
 
 
-Battery_Control::Battery_Control()
+
+BatteryWidget::BatteryWidget( float current, float max, float min ): voltage(current), maxvalue(max), minvalue(min)
 {
-	set_maxvalue(12.0);
-	set_minvalue(6.0);
-	set_value(8.0);
 	update();
 }
 
 
-float Battery_Control::get_maxvalue()
+float BatteryWidget::get_maxvalue()
 {
 	return maxvalue;
 }
 
-void Battery_Control::set_maxvalue(float voltage)
+void BatteryWidget::set_maxvalue( float maximum )
 {
-	maxvalue = voltage;
+	maxvalue = maximum;
 }
 
-float Battery_Control::get_minvalue()
+float BatteryWidget::get_minvalue()
 {
 	return minvalue;
 }
 
-void Battery_Control::set_minvalue(float voltage)
+void BatteryWidget::set_minvalue( float miniumum )
 {
-	minvalue = voltage;
+	minvalue = miniumum;
 }
 
-float Battery_Control::get_value()
+float BatteryWidget::get_value()
 {
-	return value;
+	return voltage;
 }
 
-void Battery_Control::set_value(float voltage)
+void BatteryWidget::set_voltage( float voltage_ )
 {
 	if(voltage > maxvalue)
-		value = get_maxvalue();     // maybe send some message about this situation? like "voltage above max"
+	{
+		voltage = get_maxvalue();
+	}
 	else if(voltage < minvalue)
-		value = get_minvalue();
+	{
+		voltage = get_minvalue();
+	}
 	else
-	value = voltage;
+	{
+		voltage = voltage_;
+	}
 
 	update();
 	gui.show();
 }
 
-int Battery_Control::return_procent()
+int BatteryWidget::return_percent()
 {
-	return ((value - minvalue) / ( maxvalue - minvalue))*100;
+	if( minvalue != maxvalue && minvalue < maxvalue )
+	{
+		return ( (voltage - minvalue) / ( maxvalue - minvalue) ) * 100;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-void Battery_Control::update()
+void BatteryWidget::update()
 {
-	gui.max->setText(QString::number(get_maxvalue()) + " V");
-	gui.min->setText(QString::number(get_minvalue()) + " V");
-	gui.actual->setText(QString::number(get_value()) + " V");
-	gui.status->procent = return_procent();
+	gui.max->setText( QString::number(get_maxvalue()) + " V" );
+	gui.min->setText( QString::number(get_minvalue()) + " V" );
+	gui.actual->setText( QString::number(get_value()) + " V" );
+	gui.status->percentage = return_percent();
 }
 
 
 
-Batterybar::Batterybar(QWidget *parent)
+BatteryBar::BatteryBar( QWidget *parent )
 {
-	wid = 35;
+	width_ = 35;
 	margin = 2;
 }
 
-Batterybar::~Batterybar()
+BatteryBar::~BatteryBar()
 {
-	;	//empty destructor
+	;
 }
 
 
-Qt::Alignment Batterybar::getalignment() const
+Qt::Alignment BatteryBar::getalignment() const
 {
 	return alignment_;
 }
 
-void Batterybar::setAlignment(Qt::Alignment alignment)
+void BatteryBar::setAlignment( Qt::Alignment alignment )
 {
 	alignment_ = alignment;
 	update();
 }
 
 
-void Batterybar::paintEvent(QPaintEvent *event)
+void BatteryBar::paintEvent( QPaintEvent *event )
 {
-	QPainter mypainter(this);
-	mypainter.setRenderHint(QPainter:: Antialiasing);
+	QPainter mypainter( this );
+	mypainter.setRenderHint( QPainter:: Antialiasing );
 
 	int x = 0, y = 0;
 		if ( alignment_ & Qt::AlignLeft )
 			x = 0;
 		else if ( alignment_ & Qt::AlignRight )
-			x = width() - wid;
+			x = width() - width_;					//if there is no underscore there is error that expression cannot be used as function
 		else if ( alignment_ & Qt::AlignHCenter )
-			x = (width() - wid)/2;
+			x = (width() - width_)/2;
 		else if ( alignment_ & Qt::AlignJustify )
 			x = 0;
 
@@ -180,27 +191,27 @@ void Batterybar::paintEvent(QPaintEvent *event)
 		else if ( alignment_ & Qt::AlignVCenter )
 			y = (height())/2;
 
-	QBrush background(Qt::gray,Qt::SolidPattern);
-	mypainter.setBrush(background);
-	mypainter.drawRoundedRect(x,y,wid,height(),10,10);		// background
+	QBrush background( Qt::gray,Qt::SolidPattern );
+	mypainter.setBrush( background );
+	mypainter.drawRoundedRect( x,y,width_,height(),10,10 );		// background
 
-	if(procent > 25)
+	if( percentage > 25 )
 	{
-		QBrush progresbar(Qt::green,Qt::SolidPattern);
-		mypainter.setBrush(progresbar);
+		QBrush progressbar( Qt::green,Qt::SolidPattern );
+		mypainter.setBrush( progressbar );
 	}
 	else
 	{
-		QBrush progresbar(Qt::red,Qt::SolidPattern);
-		mypainter.setBrush(progresbar);
+		QBrush progressbar( Qt::red,Qt::SolidPattern );
+		mypainter.setBrush( progressbar );
 	}
 
-	int temp = (height()) * ((float)procent/100);	// calculate how height in px should be progress bar rectangle
+	int temp = (height()) * ((float)percentage / 100);	// calculate how height in px should be progressbar rectangle
 	int halfmargin = margin / 2;
 	int y1 = height() - temp + halfmargin;		// calculate where it should start drawing on y
-	mypainter.drawRoundedRect(x+halfmargin, y1 ,wid-margin, temp-margin,10,10);
+	mypainter.drawRoundedRect( x+halfmargin, y1, width_ - margin, temp - margin, 10, 10 );
 
-	if(procent > 48)
+	if( percentage > 48 )
 	{
 		QPen textpen(Qt::black);
 		mypainter.setPen(textpen);
@@ -210,7 +221,7 @@ void Batterybar::paintEvent(QPaintEvent *event)
 		QPen textpen(Qt::white);
 		mypainter.setPen(textpen);
 	}
-	mypainter.drawText(width()/2 - wid/2 + margin, height()/2 ,30,30,0,QString::number(procent)  + "%");
+	mypainter.drawText( width()/2 - width_/2 + margin, height()/2 ,30,30,0,QString::number(percentage)  + "%" );
 }
 
 
