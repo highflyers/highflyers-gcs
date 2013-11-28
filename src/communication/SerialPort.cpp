0687e4a4c3db642c41b3d2b9e5a8fa86db561258
Paweł Matyszok <pawel.matyszok@gmail.com>
2013-11-28 19:02:40 +0100

    added setters with boost <-> our code translations

--- src/communication/SerialPort.cpp
+++ src/communication/SerialPort.cpp
@@ -17,39 +17,17 @@ SerialPort::SerialPort(string dev, int baud)
 {
 	name = dev;
 	baud_rate = baud;
-	switch (baud)
-	{
-	case     50:	baud =     B50 ; break ;
-	case     75:	baud =     B75 ; break ;
-	case    110:	baud =    B110 ; break ;
-	case    134:	baud =    B134 ; break ;
-	case    150:	baud =    B150 ; break ;
-	case    200:	baud =    B200 ; break ;
-	case    300:	baud =    B300 ; break ;
-	case    600:	baud =    B600 ; break ;
-	case   1200:	baud =   B1200 ; break ;
-	case   1800:	baud =   B1800 ; break ;
-	case   2400:	baud =   B2400 ; break ;
-	case   4800:	baud =   B4800 ; break ;
-	case   9600:	baud =   B9600 ; break ;
-	case  19200:	baud =  B19200 ; break ;
-	case  38400:	baud =  B38400 ; break ;
-	case  57600:	baud =  B57600 ; break ;
-	case 115200:	baud = B115200 ; break ;
-	case 230400:	baud = B230400 ; break ;
-	default:
-		throw exception();
-	}
 	opened = false;
+	port = shared_ptr<boost::asio::serial_port>(new boost::asio::serial_port(io_service));
 }
 
 
 bool SerialPort::open_port()
 {
-	if (port)
+	if (port->is_open())
 		return true;
 	boost::system::error_code error;
-	port = shared_ptr<boost::asio::serial_port>(new boost::asio::serial_port(io_service));
+
 
 	port->open(name, error);
 	if (error)
@@ -139,16 +117,16 @@ bool SerialPort::get_parity( SerialPort::Parity& out_parity )
 {
 	if (port->is_open())
 	{
-		boost::asio::serial_port_base::parity parity;
+		boost_parity parity;
 
 		if (!try_get_option( parity ) )
 			return false;
 
 		switch (parity.value())
 		{
-			case parity.even: out_parity = Parity::Even; break;
-			case parity.none: out_parity = Parity::None; break;
-			case parity.odd: out_parity = Parity::Odd; break;
+			case boost_parity::even: out_parity = Parity::Even; break;
+			case boost_parity::none: out_parity = Parity::None; break;
+			case boost_parity::odd: out_parity = Parity::Odd; break;
 		}
 	}
 	return false;
@@ -157,16 +135,16 @@ bool SerialPort::get_stop_bits( SerialPort::StopBits& out_stop_bits )
 {
 	if (port->is_open())
 	{
-		boost::asio::serial_port_base::stop_bits bits;
+		boost_bits bits;
 
 		if ( ! try_get_option( bits ) )
 			return false;
 
 		switch (bits.value())
 		{
-			case bits.one: out_stop_bits = StopBits::One; break;
-			case bits.onepointfive: out_stop_bits = StopBits::OnePointHalf; break;
-			case bits.two: out_stop_bits = StopBits::Two; break;
+			case boost_bits::one: out_stop_bits = StopBits::One; break;
+			case boost_bits::onepointfive: out_stop_bits = StopBits::OnePointHalf; break;
+			case boost_bits::two: out_stop_bits = StopBits::Two; break;
 		}
 		return true;
 	}
@@ -190,19 +168,84 @@ bool SerialPort::get_flow_control( SerialPort::FlowControl& out_flow_control )
 {
 	if (port->is_open())
 	{
-		boost::asio::serial_port_base::flow_control flow;
+		boost_flow flow;
 
 		if (! try_get_option( flow ))
 			return false;
 
 		switch (flow.value())
 		{
-			case flow.hardware: out_flow_control = FlowControl::Hardware; break;
-			case flow.software: out_flow_control = FlowControl::Software; break;
-			case flow.none: out_flow_control = FlowControl::None; break;
+			case boost_flow::hardware: out_flow_control = FlowControl::Hardware; break;
+			case boost_flow::software: out_flow_control = FlowControl::Software; break;
+			case boost_flow::none: out_flow_control = FlowControl::None; break;
 		}
 		return true;
 	}
 	return false;
+}
+
+void SerialPort::set_baud_rate( unsigned int baud_rate )
+{
+	if (!port)
+		throw_init_error();
+
+	try_set_option( boost::asio::serial_port_base::baud_rate( baud_rate ) );
+}
+
+void SerialPort::set_parity(Parity parity)
+{
+	if (!port)
+		throw_init_error();
+
+	boost_parity b_parity;
+
+	switch (parity)
+	{
+		case Parity::Even : b_parity = boost_parity(boost_parity::even); break;
+		case Parity::Odd : b_parity = boost_parity(boost_parity::odd); break;
+		case Parity::None : b_parity = boost_parity(boost_parity::none); break;
+	}
+	try_set_option( b_parity );
+}
+
+void SerialPort::set_stop_bits( StopBits stop_bits )
+{
+	if(!port)
+		throw_init_error();
+
+	boost_bits b_bits;
+
+	switch (stop_bits)
+	{
+		case StopBits::One : b_bits = boost_bits(boost_bits::one); break;
+		case StopBits::OnePointHalf : b_bits = boost_bits(boost_bits::onepointfive); break;
+		case StopBits::Two : b_bits = boost_bits(boost_bits::two); break;
+	}
+	try_set_option( b_bits );
+}
+
+void SerialPort::set_character_size(unsigned char char_size)
+{
+	if (!port)
+		throw_init_error();
+
+	try_set_option(boost::asio::serial_port_base::character_size(char_size));
+}
+
+void SerialPort::set_flow_control(FlowControl flow_control)
+{
+	if (!port)
+		throw_init_error();
+
+	boost_flow b_flow;
+
+	switch(flow_control)
+	{
+		case FlowControl::Hardware : b_flow = boost_flow(boost_flow::hardware); break;
+		case FlowControl::Software : b_flow = boost_flow(boost_flow::software); break;
+		case FlowControl::None : b_flow = boost_flow(boost_flow::none); break;
+	}
+
+	try_set_option(b_flow);
 }
 
