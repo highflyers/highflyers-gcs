e1a63e24b73ec63ff44598d4053fd879d2fa1e21
Maciej Smolarczyk <brayant@op.pl>
2013-12-01 21:30:00 +0100

    new errors with negative numbers, wrong max and min, big numbers

--- test/telemetry_controls/Battery.cpp
+++ test/telemetry_controls/Battery.cpp
@@ -37,3 +37,84 @@ TEST( BatteryTest, CheckReturn_procentForZeros )
 
 
 }
+
+TEST( BatteryTest, CheckReturn_procentForBigNumber )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( 999999999999 );
+	bat.set_minvalue( 999999999997 );
+	bat.set_value( 999999999998 );
+	int expected = 50;
+
+	ASSERT_EQ( bat.return_procent(), expected );
+}
+
+TEST( BatteryTest, CheckReturn_procentForWideRange )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( 999999999999 );
+	bat.set_minvalue( -999999999999 );
+	bat.set_value( 0 );
+	int expected = 50;
+	ASSERT_EQ( bat.return_procent(), expected );
+
+}
+
+TEST( BatteryTest, CheckReturn_procentWithUpSideDownWideRange )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( -999999999999 );
+	bat.set_minvalue( 999999999999 );
+	bat.set_value( 0 );
+	int expected = 50;
+	ASSERT_EQ( bat.return_procent(), expected );
+}
+
+TEST( BatteryTest, CheckReturn_procentForMaxLowerThanMin )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( -1 );
+	bat.set_minvalue( 1 );
+	bat.set_value( 0 );
+	int expected = 50;
+	ASSERT_EQ( bat.return_procent(), expected );
+}
+
+TEST( BatteryTest, ChechkReturn_procentForZeroMaxMinAndDiffValue )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( 0 );
+	bat.set_minvalue( 0 );
+	bat.set_value( 1 );
+	int expected = 0;
+	ASSERT_EQ( bat.return_procent(), expected );
+}
+
+TEST( BatteryTest, CheckReturn_procentForNegativeNumbers )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( -3 );
+	bat.set_minvalue( -10 );
+	bat.set_value( -4 );
+	float expected = ( 600/7 );
+	ASSERT_EQ( bat.return_procent(), expected );
+}
+
+TEST( BatteryTest, CheckReturn_procentForWrongNegativeNumbers )
+{
+	Battery_Control bat;
+	bat.set_maxvalue( -10 );
+	bat.set_minvalue( -2 );
+	bat.set_value( 1 );
+	float expected = -( 75/2 );
+	ASSERT_EQ( bat.return_procent(), expected );
+}
+
+
+
+
+
+
+
+
+
