45d94af0e5f4f58609c8647e9f9f68520c3bf298
Marcin Kolny <marcin.kolny@gmail.com>
2013-12-05 18:33:15 +0100

    added tempate-base logger class

--- src/core/include/core/Logger.h
+++ src/core/include/core/Logger.h
@@ -1,49 +1,88 @@
 /*
  * Logger.h
  *
- *  Created on: 30 lis 2013
+ *  Created on: 5 gru 2013
  *      Author: Marcin Kolny
  */
 
-#ifndef LOGGER_H_
-#define LOGGER_H_
+#ifndef LOG_H_
+#define LOG_H_
 
-#include <fstream>
+#include <string>
+#include <ostream>
+#include <map>
+#include <mutex>
 
-class Logger
+enum LogLevel
 {
-public:
-	enum LogLevel
-	{
-		NONE = 0,
-		INFO = 1,
-		WARNING = 2,
-		ERROR = 4,
-		ALL = 7
-	};
-
-private:
-	int info_cnt;
-	int warning_cnt;
-	int error_cnt;
-	int file_cnt;
-
-	bool use_date;
-	bool write_summary;
-	LogLevel level_mask;
-	std::string file_pattern;
-	int max_file_size;
-
-	std::ofstream file;
-
-	void reopen_file();
-	std::string generate_filename();
-
-public:
-	Logger(const std::string& file_pattern);
-	virtual ~Logger();
-
-	void log(const std::string& text, LogLevel level);
+	NONE = 0,
+	ERROR = 1,
+	WARNING = 2,
+	INFO = 4,
+	DEBUG = 8,
+	ALL = 15
 };
 
-#endif /* LOGGER_H_ */
+class NoMtSafer
+{};
+
+class MtSafer
+{
+private:
+	std::mutex m;
+public:
+	MtSafer() { m.lock(); }
+	virtual ~MtSafer() { m.unlock(); }
+};
+
+template<typename OUT_POLICY=std::ostream, LogLevel LOG_LEVEL=INFO, typename THREAD_POLICY=MtSafer>
+class Logger
+{
+private:
+	static std::map<LogLevel, std::string> log_level_str;
+
+	OUT_POLICY* out_stream;
+
+	template<LogLevel CL, typename OP, bool val>
+	struct Printer;
+
+	struct DummyOutput : public OUT_POLICY
+	{
+		template<typename T>
+		DummyOutput& operator<<( T ) { return *this; }
+	};
+
+	template<LogLevel CL, typename OP>
+	struct Printer<CL, OP, false>
+	{
+		static OP& print( OP& out )
+		{
+			static DummyOutput dummy;
+			return dummy;
+		}
+	};
+
+	template<LogLevel CL, typename OP>
+	struct Printer<CL, OP, true>
+	{
+		static OP& print(OP& out)
+		{
+			out << generate_head_of_line<CL>();
+			return out;
+		}
+	};
+
+	template<LogLevel CURRENT_LEVEL>
+	static std::string generate_head_of_line();
+
+	public:
+	Logger(OUT_POLICY* out_stream):out_stream(out_stream){}
+
+	template<LogLevel CURRENT_LEVEL>
+	OUT_POLICY& get();
+	LogLevel get_level() const;
+};
+
+#include "logger/Logger.hpp"
+
+#endif /* LOG_H_ */
