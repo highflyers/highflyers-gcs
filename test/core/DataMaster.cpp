d3b4f17f9d73cf4b128f99bc7cec20b8649c7daf
Marcin Kolny <marcin.kolny@gmail.com>
2014-12-12 01:39:13 +0100

    build: updated mailing list

--- test/core/DataMaster.cpp
+++ test/core/DataMaster.cpp
@@ -61,28 +61,3 @@ TEST_F(DataMasterTest, OneMoreSettingValueTest)
 	ASSERT_STREQ( "test", root->get_item( "/very/complex/tree" )->get_value<std::string>("variable").c_str() );
 }
 
-TEST_F(DataMasterTest, dupa)
-{
-	/*auto root = DataMaster::get_root();
-	root->register_var<int>( "sample_var" );
-
-	root->set_value( "sample_var", 12 );
-
-	ASSERT_EQ( 12, root->get_value<int>( "sample_var" ) );
-
-	auto item = root->register_item( "sample_item" );
-
-	ASSERT_EQ( item, root->get_item( "sample_item" ) );
-
-	item->register_var<std::string>( "sample_var" );
-	item->set_value<std::string>( "sample_var", "test" );
-
-	ASSERT_STREQ( "test", item->get_value<std::string>( "sample_var" ).c_str() );
-
-	root->get_item( "this/is/sparta/" )->register_var<char>( "sample_var" );
-	root->get_item( "this/is" )->get_item( "sparta" )->set_value<char>( "sample_var", 94 );
-	ASSERT_EQ( 94, root->get_item( "this" )->get_item( "is" )->get_item( "sparta" )->get_value<char>( "sample_var" ) );
-
-	ASSERT_EQ( root->get_item( "/this/is" ), root->get_item( "this" )->get_item( "/this/is" ) );*/
-}
-
